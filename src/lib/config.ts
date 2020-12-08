cd030c81be63b6e94b82f90e0e6344038a474905
imagoiq <git.noreply@posteo.no>
2020-12-08 10:14:54 +0100

    Add an error info when error arise with loading or reading tasks file

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -23,6 +23,7 @@ const getTasks = async () => {
         return Object.entries(doc.tasks);
 
     } catch (e) {
+        console.log('Error with the loading or reading of the tasks.yml file:');
         console.log(e);
         process.exit(1);
     }
